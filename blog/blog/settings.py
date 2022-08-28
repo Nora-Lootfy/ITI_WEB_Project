a95805a9d6b4d95845d0f676a6350b0b19358815
Nourhan-Abdelaziz <nourhan33abdelaziz@gmail.com>
2022-08-28 23:20:02 +0200

    adding media dir

--- blog/blog/settings.py
+++ blog/blog/settings.py
@@ -134,3 +134,6 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 LOGOUT_REDIRECT_URL = "home"
 LOGIN_REDIRECT_URL = "home"
 LOGIN_URL = "login"
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
