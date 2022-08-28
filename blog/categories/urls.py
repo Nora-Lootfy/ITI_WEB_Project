9bd585d2d5ea758eb85d9ed67427274932456442
Nourhan-Abdelaziz <nourhan33abdelaziz@gmail.com>
2022-08-28 23:45:32 +0200

    adding urls.py

---
+++ blog/categories/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from .views import PostList, PostDetail, PostCreate, PostUpdate
+urlpatterns = [
+    path('posts/', PostList.as_view(), name='posts-index'),
+    path('posts/<int:pk>', PostDetail.as_view(), name='post-details'),
+    path('createpost', PostCreate.as_view(), name='create-post'),
+    path('editpost/<int:pk>', PostUpdate.as_view(), name='edit-post'),
+]
