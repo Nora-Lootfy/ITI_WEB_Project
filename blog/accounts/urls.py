c3afb35d7622c46f365f3ca951b24e0007a3b14d
Nourhan-Abdelaziz <nourhan33abdelaziz@gmail.com>
2022-08-28 18:12:23 +0200

    update urls

--- blog/accounts/urls.py
+++ blog/accounts/urls.py
@@ -4,8 +4,12 @@ from . import views
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-    path('register/', views.register, name='register'),
-    path('logout/', auth_views.LogoutView.as_view(), name="logout"),
-    path('login/', auth_views.LoginView
-        .as_view(template_name="accounts/login.html"), name="login")
+     path('register/', views.register, name='register'),
+     path('logout/', auth_views.LogoutView.as_view(), name="logout"),
+     path('login/', auth_views.LoginView
+          .as_view(template_name="accounts/login.html"), name="login"),
+     path('changepassword', auth_views.PasswordChangeView.as_view(template_name="accounts/changepassword.html"),
+          name='change_password'),
+     path('changepassword/done', auth_views.PasswordChangeDoneView.as_view(template_name="accounts/changepassworddone.html"),
+          name='password_change_done'),
 ]
