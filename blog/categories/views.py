1d6025e496f681b01b1f6d09f5bc4e8eae090a62
Nourhan-Abdelaziz <nourhan33abdelaziz@gmail.com>
2022-08-28 23:50:40 +0200

    adding categories views

--- blog/categories/views.py
+++ blog/categories/views.py
@@ -1,3 +1,33 @@
 from django.shortcuts import render
-
+from django.urls import reverse_lazy
+# from django.http import HttpResponse
+from django.views.generic import ListView, DetailView, CreateView, DeleteView, UpdateView
+from .models import Post
+from .forms import PostForm
 # Create your views here.
+
+
+# def posts_info(request):
+#     return render(request, 'posts/posts_index.html')
+
+class PostList(ListView):
+    model = Post
+    template_name = 'posts/posts_index.html'
+
+class PostDetail(DetailView):
+    model = Post
+    template_name = 'posts/post_details.html'
+
+class PostCreate(CreateView):
+    form_class = PostForm
+    template_name = 'posts/create_post.html'
+    success_url = 'posts/posts_index.html'
+
+
+class PostUpdate(UpdateView):
+    model = Post
+    form_class = PostForm
+    template_name = 'posts/update_post.html'
+    def get_success_url(self):
+        post_id = self.object.id
+        return reverse_lazy('post-details', kwargs={'pk': post_id})
