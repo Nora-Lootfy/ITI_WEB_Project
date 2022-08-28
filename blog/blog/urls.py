bbbaec167a1c9e1ee19cf13d552c6641499bdcef
Nourhan-Abdelaziz <nourhan33abdelaziz@gmail.com>
2022-08-29 00:01:28 +0200

    modify urls

--- blog/blog/urls.py
+++ blog/blog/urls.py
@@ -21,7 +21,7 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("main.urls")),
     path("accounts/", include("accounts.urls")),
-    path('', include('posts.urls')),
+    path("categories/", include("categories.urls")),
 
 ]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
