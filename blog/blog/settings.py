a2a4984cf22dd7c056798c31b7ceaba95f8fa305
ahmedalaa <ahmedalaa1316@gmail.com>
2022-10-03 11:46:33 +0200

    hosting update

--- blog/blog/settings.py
+++ blog/blog/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-t@_=pjf6pc$1jgekhcynblv=hv=cpc5f4mt3r44n3pcc9^5gzt"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
@@ -50,6 +50,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
--- blog/blog/urls.py
+++ blog/blog/urls.py
@@ -4,10 +4,19 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
+from django.views.static import serve
+from django.conf.urls import url
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("main.urls")),
     path("accounts/", include("accounts.urls")),
     path("categories/", include("categories.urls")),
+
+    url(r'^media/(?P<path>.*)$', serve,
+        {'document_root':       settings.MEDIA_ROOT}),
+    url(r'^static/(?P<path>.*)$', serve,
+        {'document_root': settings.STATIC_ROOT}),
+
+
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
